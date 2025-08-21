bca677a656ef03af18a6e5006c6ce9e86d9ac2bd
Aromalsathees <aromalsatheesh802003@gmail.com>
2025-08-21 15:43:01 +0530

    initial Commit

---
+++ INT-44.js
@@ -0,0 +1,76 @@
+// FRESHERS INTERVIEW - REACT JS, JAVASCRIPT -44 //
+
+//1//
+// const name = 'aromal'
+// console.log(name())
+
+
+//22//
+// console.log([] == [])
+
+
+//3//
+// function abc(x,y,z){
+
+// }
+// function xyz(a,b=0,c){
+
+// }
+// function pqr(m,n,x,a=0,b,c){
+
+// }
+// console.log(abc.length) //3 ans//
+// console.log(xyz.length)  //1//
+// console.log(pqr.length)  //3//
+
+
+// 4 //
+// let abc = 'its a string'
+// function fn(){
+//     console.log(abc)
+//      let abc = 'ajmal'
+// }
+// fn()
+
+
+// 5
+// Learn Arrow function , Regular function , hoisting , currying , infinit currying ,
+
+
+//6
+// if you want to add javascript Globally in React Js ,what will we do ?
+
+
+
+// 7
+// abc()
+// var abc = function(){
+//     console.log('am first')
+// }
+// abc()
+// function abc(){
+//     console.log('am last')
+// }
+// abc()
+
+//8
+//  what's Optional Chaining ? //
+
+
+
+//9//
+// let stri = "this is my interview";
+// OUtput : This Is My Interview
+
+
+// 10
+// Fetch and Axios differnce ?
+// Requeat cancalation in both cases ?
+// in axios , how u transform the server response data?
+// have you done with using CDN?
+// package.json and package.lockson differnce?
+// tild operator and carror operator? and differnce between them ?
+
+
+
+
